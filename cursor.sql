1b0b594612b14dee67e650c43c4d042a7af3f27d
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-03-10 12:06:36 -0500

    Cursor terminado

--- cursor.sql
+++ cursor.sql
@@ -1,27 +1,33 @@
-/*Realizar un cursor que me permita saber las brigadas que han sido castigadas por calificación regular*/
+/*Realizar un cursor que me permita saber las brigadas que han sido castigadas por calificación regular, cuántas veces han sido castigadas y el total de brigradas castigadas*/
 
 DELIMITER //
 
 CREATE PROCEDURE cursor_brigadas()
 
 BEGIN
-    declare done int default 0;
+    DECLARE done INT DEFAULT 0;
+    DECLARE count BIGINT DEFAULT 0;
     DECLARE batallon VARCHAR(20);
-    DECLARE batallones CURSOR FOR SELECT `BATALLON_ALIAS` FROM `entrenamiento`,`BATALLON` WHERE entrenamiento.`CALIFICACION_ID` = 1 AND BATALLON.`BATALLON_ID` = entrenamiento.`BATALLON_ID` GROUP BY BATALLON.`BATALLON_ID`;
+    DECLARE veces INT;
+    DECLARE batallones CURSOR FOR SELECT `BATALLON_ALIAS` AS BATALLON, COUNT(*) AS VECES_CASTIGADAS FROM `entrenamiento`,`BATALLON` WHERE entrenamiento.`CALIFICACION_ID` = 1 AND BATALLON.`BATALLON_ID` = entrenamiento.`BATALLON_ID` GROUP BY BATALLON.`BATALLON_ID`;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
     
     OPEN batallones;
 
     simple_loop: LOOP
 
-        FETCH batallones INTO batallon;
+        FETCH batallones INTO batallon, veces;
+
         IF done THEN
             LEAVE simple_loop;
         END IF;
 
-        select batallon;
+        SELECT batallon AS NOMBRE_BATALLON, veces AS VECES_CASTIGADAS;
+        SET count=count+1;
 
     END LOOP simple_loop;
+
+    SELECT count AS TOTAL_BRIGADAS;
 
     CLOSE batallones;
     
