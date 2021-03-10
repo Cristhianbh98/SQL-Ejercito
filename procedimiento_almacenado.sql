a87390f7bff6cfc8466d88b98d33d597e3da686f
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-03-10 17:57:51 -0500

    Procedimiento Almacenado terminado

--- procedimiento_almacenado.sql
+++ procedimiento_almacenado.sql
@@ -1,2 +1,22 @@
 /*Realizar un procedimiento almacenado que retorne los nombres de los integrantes de una brigrada ingresada*/
 
+DELIMITER //
+
+CREATE PROCEDURE integrantes_brigadas(IN s_batallon VARCHAR(20))
+
+BEGIN
+    DECLARE id INT;
+    DECLARE batallon CURSOR FOR SELECT `BATALLON_ID` FROM `batallon` WHERE `BATALLON_ALIAS` = s_batallon;
+
+    OPEN batallon;
+
+    FETCH batallon INTO id;
+
+    SELECT CONCAT(`SOLDADO_NOMBRE`,' ',`SOLDADO_APELLIDO`) AS SOLDADO FROM `soldado` WHERE `BATALLON_ID` = id;
+
+    CLOSE batallon;
+
+END; //
+
+
+DELIMITER ;
